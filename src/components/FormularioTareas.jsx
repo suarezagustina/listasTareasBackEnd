edc76e6bdb44d6a7c23bb49b93eefedf21f2904d
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-03-15 18:17:22 -0300

    hice cambios para pruebas de error en el formulario para agregar tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -9,10 +9,13 @@ const FormularioTareas = () => {
       <h2 className="display-5 my-3 text-white">Agregar una Tarea:</h2>
       <Form>
      <Form.Control size="lg" type="text" placeholder="Titulo de tarea..."/>
-      <br />
-      <Form.Control size="sm" type="text" placeholder="Subtitulo" />
-      <br />
-      <Form.Control type="text" placeholder="Descripcion Breve" />
+     <p className="text-danger mb-0">prueba de error</p>
+      <br/>
+      <Form.Control size="sm" type="text" placeholder="Subtitulo"/>
+      <p className="text-danger mb-0">prueba de error</p>
+      <br/>
+      <Form.Control type="text" placeholder="Descripcion Breve"/>
+      <p className="text-danger mb-0">prueba de error</p>
       <Button variant="light" className="my-3 btn btn-light" type="submit">Enviar</Button>
       </Form>
     </section>
