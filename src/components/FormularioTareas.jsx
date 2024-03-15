89ddd40c3ec3c00c735ea96886e0d3ba018198ac
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-03-15 18:08:33 -0300

    instale sweet alert y react hook form

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,17 +1,20 @@
 import { Button, Container, Form } from "react-bootstrap";
+//import Swal from "sweetalert2";
 
 const FormularioTareas = () => {
-  
+
   return (
     <Container className="mainPage">
     <section className="text-center">
       <h2 className="display-5 my-3 text-white">Agregar una Tarea:</h2>
-     <Form.Control size="lg" type="text" placeholder="Titulo de tarea..." />
+      <Form>
+     <Form.Control size="lg" type="text" placeholder="Titulo de tarea..."/>
       <br />
       <Form.Control size="sm" type="text" placeholder="Subtitulo" />
       <br />
       <Form.Control type="text" placeholder="Descripcion Breve" />
       <Button variant="light" className="my-3 btn btn-light" type="submit">Enviar</Button>
+      </Form>
     </section>
     </Container>
   );
