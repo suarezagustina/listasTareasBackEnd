9f996525cce626d6b6da1db46a660d3bfadb500c
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-03-15 20:29:40 -0300

    muestro los datos de las tareas en la pagina para ver todas las tareas

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,11 +1,33 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import CardTarea from "./Tarea/CardTarea";
+import { useEffect, useState } from "react";
+import { leerTareasAPI } from "../helpers/queries";
 
 const Tareas = () => {
+    const [tareas, setTareas] = useState ([]);
+    useEffect(()=>{
+        traerTarea();
+       },[])
+       const traerTarea = async()=>{
+        try {
+         const listaTareasAPI = await leerTareasAPI()
+         setTareas(listaTareasAPI);
+        } catch (error) {
+         console.log(error)
+        }
+       }
     return (
         <>
-        <Container className="mainPage text-center p-3">
-            <p className="text-white display-5">Tareas a Realizar</p>
-        </Container>
+       <Container className='text-center mainPage'>
+     <div>
+    <h2 className='my-4 display-3 text-white'>Tareas a realizar</h2>
+    </div>
+    <Row>
+    {
+            tareas.map((tarea)=><CardTarea key={tarea.id} tarea={tarea}></CardTarea>)
+          }
+    </Row>
+    </Container>
         </>
     );
 };
